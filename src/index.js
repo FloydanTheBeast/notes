0f24d12e4ef39db83fa1fe1f6b53bd82bc63e4b6
Shadi <floydanthebeast@gmail.com>
2018-09-22 17:59:02 +0300

    Initialize database

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/App'
+import { createStore } from 'redux'
+import { Provider }  from 'react-redux'
+import CellReducer from './reducers/cells'
+import db from 'db'
 
-// Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
-let root = document.createElement('div');
-root.id = "root";
-document.body.appendChild( root );
+const store = createStore(CellReducer, db.getState())
+console.log(store.getState())
 
-// Now we can render our application into it
-render( <App />, document.getElementById('root') );
+store.subscribe(() => { 
+    console.log(store.getState())
+})
+
+let root = document.createElement('div')
+root.id = "root"
+document.body.appendChild( root)
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+ document.getElementById('root') )
