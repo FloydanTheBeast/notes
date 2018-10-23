10ac22af973fa816404feba76d78dded608c3ecd
Shadi <floydanthebeast@gmail.com>
2018-10-24 00:22:10 +0300

    Add dark theme, improve UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,47 @@
 import '../assets/styles/main.sass'
 import '../assets/styles/prism-base16-ateliersulphurpool.light.css'
 import React, { Component } from 'react'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 import CellList from '../containers/CellList'
 import MenuBar from './MenuBar'
 import 'smoothscroll-for-websites'
 
-class App extends React.Component {
+class App extends Component {
+    componentDidMount() {
+        // setTimeout(() => {
+        //     document.documentElement.setAttribute('data-theme', 'dark')
+        //     // setTimeout(() => document.documentElement.setAttribute('data-theme', ''), 3000)
+        // }, 3000)
+
+    }
+
     render() {
         return (
-            <div className='app'>
-                {/* <MenuBar /> */}
-                <div className='content'>
-                    <h1 className='app-logo'>📝 Notes</h1>
-                    <CellList />
+            <HashRouter >
+                <div className='app'>
+                    <MenuBar />
+                    <div className='content'>
+                    <Switch>
+                        <Route path='/options'>
+                            <div className='options'>
+                                <h1>User settings</h1>
+                                <h2>Theme</h2>
+                                <div className='light-theme-btn' onClick={() => document.documentElement.setAttribute('data-theme', '')}>Light theme</div>
+                                <div className='dark-theme-btn' onClick={() => document.documentElement.setAttribute('data-theme', 'dark')}>Dark theme</div>
+                            </div>
+                        </Route>
+                        <Route path='/' render={() => (
+                            <React.Fragment>
+                                <h1 className='app-logo'>📝 Notes</h1>
+                                <CellList />
+                            </React.Fragment>
+                        )}>
+                        </Route>
+                    </Switch>
+                    </div>
+                    
                 </div>
-            </div>
+            </HashRouter>
         );
     }
 }
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class MenuBar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpened: false
+        }
+    }
+
     render() {
         return(
-            <div className='menu-bar'>
-                <ul>
-                    <li>Notes</li>
-                    <li>About</li>
-                </ul>
-            </div>
+            <React.Fragment>
+                <div onClick={() => this.setState((state) => ({ isOpened: !state.isOpened }))} className='hamburger-btn'>
+                    <div className='bars'></div>
+                </div>
+                <div className={`menu-bar ${this.state.isOpened ? 'open' : ''}`}>
+                    <ul>
+                        <li>
+                            <Link to='/'>Notes</Link>
+                        </li>
+                        <li>
+                            <Link to='/options'>Options</Link>
+                        </li>
+                    </ul>
+                </div>
+            </React.Fragment>
         )
     }
 }
